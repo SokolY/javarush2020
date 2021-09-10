a592d30eea9bdaf3a592facb2fd70caba87950b0
sokoly <yura.sokol@gmail.com>
2021-09-10 18:02:50 +0300

    MinesweeperGameStage#4

--- JavaRushTasks/8.Games/src/com/javarush/games/minesweeper/GameObject.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/minesweeper/GameObject.java
@@ -3,6 +3,7 @@ package com.javarush.games.minesweeper;
 public class GameObject {
     public int x;
     public int y;
+    boolean isMine;
     public GameObject(int x, int y){
         this.x = x;
         this.y = y;
--- JavaRushTasks/8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -4,8 +4,18 @@ import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
     private static final int SIDE = 9;
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
+    private void createGame(){
+        for(int i = 0; i<gameField.length; i++){
+            for (int j = 0; j<gameField[i].length; j++){
+                gameField[j][i] = new GameObject(i, j);
+                setCellColor(j, i, Color.ORANGE);
+            }
+        }
+    }
     @Override
     public void initialize(){
         setScreenSize(SIDE, SIDE);
+        createGame();
     }
 }
