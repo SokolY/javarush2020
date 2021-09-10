82033d4887e5b20c76cfe8240f727faa1208e933
sokoly <yura.sokol@gmail.com>
2021-09-10 19:07:06 +0300

    MinesweeperGameStage#5

--- JavaRushTasks/8.Games/src/com/javarush/games/minesweeper/GameObject.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/minesweeper/GameObject.java
@@ -3,9 +3,10 @@ package com.javarush.games.minesweeper;
 public class GameObject {
     public int x;
     public int y;
-    boolean isMine;
-    public GameObject(int x, int y){
+    public boolean isMine;
+    public GameObject(int x, int y, boolean isMine){
         this.x = x;
         this.y = y;
+        this.isMine = isMine;
     }
 }
--- JavaRushTasks/8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -5,11 +5,14 @@ import com.javarush.engine.cell.*;
 public class MinesweeperGame extends Game {
     private static final int SIDE = 9;
     private GameObject[][] gameField = new GameObject[SIDE][SIDE];
+    private int countMinesOnField;
     private void createGame(){
         for(int i = 0; i<gameField.length; i++){
             for (int j = 0; j<gameField[i].length; j++){
-                gameField[j][i] = new GameObject(i, j);
+                boolean haveMine = getRandomNumber(10)<1;
+                gameField[j][i] = new GameObject(i, j, haveMine);
                 setCellColor(j, i, Color.ORANGE);
+                if(haveMine) countMinesOnField++;
             }
         }
     }
