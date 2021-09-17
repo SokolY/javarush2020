52faca1c5cb5d6d7ba52c8d8ced29ca6890c9b35
sokoly <yura.sokol@gmail.com>
2021-09-17 18:26:07 +0300

    Game2048Stage#5

--- JavaRushTasks/8.Games/src/com/javarush/games/game2048/Game2048.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -4,9 +4,37 @@ import com.javarush.engine.cell.*;
 
 public class Game2048 extends Game {
     private static final int SIDE =4;
+    private int[][]gameField = new int [SIDE][SIDE];
 
     @Override
     public void initialize() {
        setScreenSize(SIDE, SIDE);
+       createGame();
+        drawScene();
     }
+        private void createGame(){
+            createNewNumber();
+            createNewNumber();
+    }
+    private void drawScene(){
+        for (int y = 0; y<gameField.length; y++){
+            for (int x = 0; x<gameField[y].length; x++){
+                setCellColor(x, y, Color.RED);
+            }
+        }
+    }
+    private void createNewNumber(){
+        int randX = getRandomNumber(SIDE);
+        int randY = getRandomNumber(SIDE);
+        int randNumber = getRandomNumber(10);
+        if (gameField[randY][randX]!=0){
+            createNewNumber();
+        }
+        else {
+            if (randNumber == 9) {
+                gameField[randY][randX] = 4;
+            } else gameField[randY][randX] = 2;
+        }
+    }
+
 }
