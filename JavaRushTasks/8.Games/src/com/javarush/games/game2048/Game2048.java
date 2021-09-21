1974390af72f5d464414ed50349967f253c1b609
sokoly <yura.sokol@gmail.com>
2021-09-21 18:39:33 +0300

    Game2048Stage#7

--- JavaRushTasks/8.Games/src/com/javarush/games/game2048/Game2048.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -2,6 +2,8 @@ package com.javarush.games.game2048;
 
 import com.javarush.engine.cell.*;
 
+import java.util.Arrays;
+
 public class Game2048 extends Game {
     private static final int SIDE =4;
     private int[][]gameField = new int [SIDE][SIDE];
@@ -40,16 +42,26 @@ public class Game2048 extends Game {
     private Color getColorByValue(int value){
        Color returnColor = null;
         switch (value) {
-            case 0: return Color.WHITE;
-            case 2: return Color.LIGHTPINK;
-            case 4: return Color.BLUEVIOLET;
-            case 8: return Color.BLUE;
-            case 16: return Color.CYAN;
-            case 32: return Color.LIGHTSEAGREEN;
-            case 64: return Color.LIMEGREEN;
-            case 128: return Color.ORANGE;
-            case 256: return Color.INDIANRED;
-            case 512: return Color.RED;
+            case 0: returnColor = Color.WHITE;
+                break;
+            case 2: returnColor = Color.AZURE;
+                    break;
+            case 4: returnColor = Color.BEIGE;
+                break;
+            case 8: returnColor = Color.BLUE;
+                 break;
+            case 16: returnColor = Color.BLUEVIOLET;
+                    break;
+            case 32: returnColor = Color.CHARTREUSE;
+                break;
+            case 64: returnColor = Color.CORAL;
+                 break;
+            case 128: returnColor = Color.CORNSILK;
+                    break;
+            case 256: returnColor = Color.DARKCYAN;
+                break;
+            case 512: returnColor = Color.YELLOW;
+                break;
             case 1024: returnColor = Color.YELLOWGREEN;
                 break;
             case 2048: returnColor = Color.GOLD;
@@ -87,5 +99,23 @@ public class Game2048 extends Game {
                 break;
         }
 
+    }
+    private boolean compressRow(int[] row){
+        int[] originalRow = Arrays.copyOf(row, row.length);
+
+        for (int i = 0; i<row.length-1; i++){
+            for(int j=i+1; j<row.length; j++){
+                if (row[j]!=0 && row[i] == 0){
+                    int buf = row[i];
+                    row[i] = row[j];
+                    row[j] = buf;
+                }
+            }
+
+        }
+        if(Arrays.equals(originalRow, row)){
+            return false;
+        }
+        else return true;
     }
 }
